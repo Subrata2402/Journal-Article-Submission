dc5f09d01095e4ab63e9c28d8fd8ebbbc022f130
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2025-06-15 17:35:24 +0530

    added preloader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 import Spinner from './components/common/Spinner';
+import Preloader from './components/common/Preloader';
 import './assets/styles/main.scss';
 
 // Global loading component for suspense fallback
@@ -16,27 +17,30 @@ const GlobalLoader = () => (
 
 const App = () => {
   return (
-    <Suspense fallback={<GlobalLoader />}>
-      <AuthProvider>
-        <Router>
-          <AppRoutes />
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
-            closeButton={false}
-            draggablepercent={80}
-          />
-        </Router>
-      </AuthProvider>
-    </Suspense>
+    <>
+      <Preloader />
+      <Suspense fallback={<GlobalLoader />}>
+        <AuthProvider>
+          <Router>
+            <AppRoutes />
+            <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+              closeButton={false}
+              draggablepercent={80}
+            />
+          </Router>
+        </AuthProvider>
+      </Suspense>
+    </>
   );
 };
 
